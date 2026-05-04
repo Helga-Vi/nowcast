0e214995e8ae71f3c5e454ce03e5fca644876be1
Helga-Vi <katarina.jorgensen@gmail.com>
2026-05-04 15:32:54 +0200

    Initial API call

--- src/WeatherPage.tsx
+++ src/WeatherPage.tsx
@@ -1,8 +1,19 @@
 // My page
+import { useEffect } from "react";
+
 export default function WeatherPage() {
-  return (
-    <div>
-      <h1>Nowcast is running</h1>
-    </div>
-  );
+  useEffect(() => {
+    async function fetchWeather() {
+      const res = await fetch(
+        "https://api.met.no/weatherapi/locationforecast/2.0/compact?lat=60.39&lon=5.32"
+      );
+
+      const data = await res.json();
+      console.log("WEATHER DATA:", data);
+    }
+
+    fetchWeather();
+  }, []);
+
+  return <div>Nowcast is running</div>;
 }
